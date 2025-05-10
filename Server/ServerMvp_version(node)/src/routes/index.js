ba78ef33a611edaf8ab184c958eedb355c2d34f2
Tim <148056216+Tima126@users.noreply.github.com>
2025-05-10 20:02:45 +0300

    Update index.js

--- Server/ServerMvp_version(node)/src/routes/index.js
+++ Server/ServerMvp_version(node)/src/routes/index.js
@@ -1,11 +1,11 @@
-// src/routes/index.js
+
 const express = require('express');
 const router = express.Router();
 
-// Импортируем маршруты напрямую
+
 const skillRoutes = require('./api/v1/skill.routes');
 
-// Регистрируем маршруты
-router.use('/v1/skills', skillRoutes); // ← теперь здесь указан полный путь
+
+router.use('/v1/skills', skillRoutes); 
 
 module.exports = router;
